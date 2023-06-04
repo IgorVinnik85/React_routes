4a123996540f270584e2ff3ac307fa832a7a963b
Volodymyr <mv.design8@gmail.com>
2023-06-04 16:41:16 +0300

    step-1

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,29 @@
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
+import { useState } from 'react';
 
-export const SearchForm = () => {
+export const SearchForm = ({ queary }) => {
+  const [value, setValue] = useState('');
+  const handleChange = e => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    queary(value);
+  };
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={handleSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
+      <Select
+        aria-label="select"
+        name="region"
+        required
+        onChange={handleChange}
+      >
+        <option disabled defaultValue="">
           Select a region and press enter
         </option>
         <option value="africa">Africa</option>
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,39 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useEffect, useState } from 'react';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  const [search, setSearch] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [countries, setCountries] = useState([]);
+  
+  useEffect(() => {
+    if (!search) return
+    setLoading(true);
+    const regionCountries = async () => {
+      try {
+        const data = await fetchByRegion(search);
+        setCountries(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    regionCountries();
+  }, [search]);
+
+  console.log('search :>> ', search);
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        {error && <Heading>Something go's wrong {error} </Heading>}
+        {loading && <Loader />}
+        <SearchForm queary={setSearch} />
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
